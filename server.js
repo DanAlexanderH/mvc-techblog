1d1655d99d84ea6c6c06f9532238f331746691ee
Daniel <ttdanny98@gmail.com>
2023-04-08 10:45:48 -0700

    added helper javascript to haleprs folder

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const sequelize = require('./config/connection');
-// const path = require('path');
+const path = require('path');
+const routes = require('./controller');
+const exphbs = requier('express-handlebars')
 const session = require('express-session');
+const helpers = require('./helpers')
 const SequalizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
@@ -18,9 +21,16 @@ const sess = {
 
 };
 
+const hbs = exphbs.create({ helpers });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', handlebars);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session(sess));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log(`App is connected on PORT ${PORT}`))
